7f8b788bee2bc4972c436305ded67e9ab59212d1
MarciaPratas2000 <156352013+MarciaPratas2000@users.noreply.github.com>
2024-06-03 20:08:46 +0100

    Improved MainUpdate.js code

--- src/MainWeatherElements/MainUpdate.js
+++ src/MainWeatherElements/MainUpdate.js
@@ -4,8 +4,9 @@ import CurrentWeatherInfo from "./CurrentWeatherInfo.js";
 import "./Main.css";
 import ForecastWeather from "./ForecastWeather.js";
 
-export default function MainWeather(props) {
+export default function MainUpdate(props) {
   // Use useState directly and then destructure the array it returns
+  // Every time the form is submitted, the data state will be reset to null
   const [data, setData] = useState(null);
 
   const [weatherInfo, setWeatherInfo] = useState({
@@ -21,10 +22,9 @@ export default function MainWeather(props) {
   });
 
   // Update state when props.data changes
+  // And React runtime errors prevent: makes sure that props.data is not null anymore, as it is in first state even if not null in its essence.
   if (props.data !== data) {
     setData(props.data);
-  //React runtime errors prevent: makes sure that props.data is not null anymore, as it is in first state.
-    if (props.data) {
       setWeatherInfo({
         city: props.data.name || "Unknown",
         description: props.data.weather[0]?.description || "Unknown",
@@ -37,11 +37,11 @@ export default function MainWeather(props) {
         temperature: Math.round(props.data.main.temp) || 0,
         unit: "ºC",
       });
-    }
+    
   }
 
   return (
-    <div className="WeatherMain">
+    <div className="MainUpdate">
       <div className="Main">
         <main className="mainElement">
           <div className="current-weather">
