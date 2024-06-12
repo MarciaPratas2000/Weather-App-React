849f57ae5aeb28c5481f60e0ba60fd485ebaa932
MarciaPratas2000 <156352013+MarciaPratas2000@users.noreply.github.com>
2024-06-12 23:20:23 +0100

    adjusted cityForm.js

--- src/FormHandleElements/CityForm.js
+++ src/FormHandleElements/CityForm.js
@@ -1,36 +1,10 @@
 import React, { useState } from "react";
 import "./CityForm.css";
 import CurrentWeatherUpdate from "../MainWeatherElements/CurrentWeatherUpdate.js";
-import ForecastColumn from "../MainWeatherElements/ForecastColumn.js";
 
 export default function CityForm() {
-  const [city, setCity] = useState("Lisbon");
-  const [response, setResponse] = useState(null);
-  const [error, setError] = useState(null);
-  const [loaded, setLoaded] = useState(false);
-
-  const apiKey = "a969311cfcbb4a83dfad2cf7478397f9";
-
-  function updateCityname(event) {
-    setCity(event.target.value);
-  }
-
-  function handleResponse(response) {
-    setResponse(response.data);
-    setLoaded(true);
-  }
-
-  function fetchWeather(city) {
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
-    axios
-      .get(apiUrl)
-      .then(handleResponse)
-      .catch((error) => {
-        setError(error);
-        setLoaded(true);
-      });
-  }
+  const [city, setCity] = useState("Lisbon"); // Default city is Lisbon
+  const [inputValue, setInputValue] = useState("");
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -57,7 +31,6 @@ export default function CityForm() {
         </form>
        <div>
         <CurrentWeatherUpdate city={city} />
-        <ForecastColumn city={city} />
       </div>
     </div>
   );
